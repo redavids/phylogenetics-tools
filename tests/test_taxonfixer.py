a4b33a4481550b2d4baff649b324fe069c60804b
Jed Chou <jedchou1@illinois.edu>
2015-02-28 13:54:46 -0600

    changes paths for jed's local machine

--- tests/test_taxonfixer.py
+++ tests/test_taxonfixer.py
@@ -6,34 +6,34 @@ import taxonfixer
 
 class TestTreeReWrite(unittest.TestCase):
     def test_tree_rewrite1(self):
-        treelist = dendropy.TreeList.get_from_path('/Users/ruthdavidson/code/phylogenetics-tools/tests/feefifofum','newick')
+        treelist = dendropy.TreeList.get_from_path('/Users/jedchou/phylogenetics-tools/tests/feefifofum','newick')
         taxondict = {'fa': 1, 'fee': 2, 'fi' : 3, 'fo': 4, 'foo': 5, 'fum': 6}
         newlist = ['((2,3),(4,6));', '((2,6),(3,1));', '(((5,1),3),4);']
-        n,d = taxonfixer.listparse('/Users/ruthdavidson/code/phylogenetics-tools/tests/feefifofum')
+        n,d = taxonfixer.listparse('/Users/jedchou/phylogenetics-tools/tests/feefifofum')
         self.assertEqual(n,newlist)
         self.assertEqual(d,taxondict)
         
     def test_tree_rewrite2(self):
-        treelist = dendropy.TreeList.get_from_path('/Users/ruthdavidson/code/phylogenetics-tools/tests/abcdtrees','newick')
+        treelist = dendropy.TreeList.get_from_path('/Users/jedchou/phylogenetics-tools/tests/abcdtrees','newick')
         taxondict = {'a':1, 'b':2, 'c':3, 'd':4}
         newlist = ['(((1,2),3),4);','((1,3),(2,4));','((4,1),(2,3));']
-        n,d = taxonfixer.listparse('/Users/ruthdavidson/code/phylogenetics-tools/tests/abcdtrees')
+        n,d = taxonfixer.listparse('/Users/jedchou/phylogenetics-tools/tests/abcdtrees')
         self.assertEqual(n,newlist)
         self.assertEqual(d,taxondict)
 
 class TestFileSwap(unittest.TestCase):
     def test_file_rewrite(self):
-        taxonfixer.fileswap('/Users/ruthdavidson/code/phylogenetics-tools/tests/feefifofum','/Users/ruthdavidson/code/phylogenetics-tools/tests/feefifofumswap','newick')
+        taxonfixer.fileswap('/Users/jedchou/phylogenetics-tools/tests/feefifofum','/Users/jedchou/phylogenetics-tools/tests/feefifofumswap','newick')
         #d = {'fa': 1, 'fee': 2, 'fi' : 3, 'fo': 4, 'foo': 5, 'fum': 6}
-        f = open('/Users/ruthdavidson/code/phylogenetics-tools/tests/feefifofumswap','r')
+        f = open('/Users/jedchou/phylogenetics-tools/tests/feefifofumswap','r')
         s1 = f.read()
-        g = open('/Users/ruthdavidson/code/phylogenetics-tools/tests/testffffswap','r')
+        g = open('/Users/jedchou/phylogenetics-tools/tests/testffffswap','r')
         s2 = g.read()
         self.assertEqual(s1,s2)
-        h = open('/Users/ruthdavidson/code/phylogenetics-tools/tests/dictffffswap','r')
+        h = open('/Users/jedchou/phylogenetics-tools/tests/dictffffswap','r')
         Sh = h.read()
         dh = ast.literal_eval(Sh)
-        k = open('/Users/ruthdavidson/code/phylogenetics-tools/tests/feefifofum.taxon_key','r')
+        k = open('/Users/jedchou/phylogenetics-tools/tests/feefifofum.taxon_key','r')
         Sk = k.read()
         dk = ast.literal_eval(Sk)
         self.assertEqual(dh, dk)
@@ -43,17 +43,17 @@ class TestFileSwap(unittest.TestCase):
         g.close()
 
     def test_file_rewrite2(self):
-        taxonfixer.fileswap('/Users/ruthdavidson/code/phylogenetics-tools/tests/abcdtrees','/Users/ruthdavidson/code/phylogenetics-tools/tests/abcdtreesswap','newick')
+        taxonfixer.fileswap('/Users/jedchou/phylogenetics-tools/tests/abcdtrees','/Users/jedchou/phylogenetics-tools/tests/abcdtreesswap','newick')
         l =[]
-        f = open('/Users/ruthdavidson/code/phylogenetics-tools/tests/abcdtreesswap','r')
+        f = open('/Users/jedchou/phylogenetics-tools/tests/abcdtreesswap','r')
         s1 = f.read()
-        g = open('/Users/ruthdavidson/code/phylogenetics-tools/tests/testabcdswap','r')
+        g = open('/Users/jedchou/phylogenetics-tools/tests/testabcdswap','r')
         s2 = g.read()
         self.assertEqual(s1,s2)
-        h = open('/Users/ruthdavidson/code/phylogenetics-tools/tests/dictabcdswap','r')
+        h = open('/Users/jedchou/phylogenetics-tools/tests/dictabcdswap','r')
         Sh = h.read()
         dh = ast.literal_eval(Sh)
-        k = open('/Users/ruthdavidson/code/phylogenetics-tools/tests/abcdtrees.taxon_key','r')
+        k = open('/Users/jedchou/phylogenetics-tools/tests/abcdtrees.taxon_key','r')
         Sk = k.read()
         dk = ast.literal_eval(Sk)
         self.assertEqual(dh, dk)
